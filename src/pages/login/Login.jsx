cb7b2546d3e3d2cac001e8098ddecdcdad554092
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-09-16 16:32:37 +0700

    Create Route

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
-import './App.css'
+import './Login.css'
 
-function App() {
+function Login() {
 
   return (
     <>
@@ -9,4 +9,4 @@ function App() {
   )
 }
 
-export default App
+export default Login
