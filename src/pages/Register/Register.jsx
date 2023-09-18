6f4bf1f8106b13eada580dc18003572335000330
PhetployST <139663878+PhetployST@users.noreply.github.com>
2023-09-18 23:19:28 +0700

    login & register update

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,7 +4,26 @@ import './Register.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+<>
+  <div>
+		<form>
+			<h1>Create Account</h1>
+			<span>Please enter your Username password and Email!</span>
+			<input type="userName" placeholder="Username" />
+			<input type="fullName" placeholder="Fullname" />
+			<input type="password" placeholder="Password" />
+      <input type="confirmPassword" placeholder="Confirm password" />
+			<button>Sign Up</button>
+      <span href="#">already have an account? Login</span>
+		</form>
+	</div>
+
+  <div>
+				<h1>Hello, Friend!</h1>
+				<p>Enter your personal details and start journey with us</p>
+				<button id="signUp">Sign Up</button>
+	</div>
+</>
   )
 }
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,10 +3,25 @@ import './Login.css'
 function Login() {
 
   return (
-    <>
-      <h1 className='uppercase underline text-xl'>hello world</h1>
-      <h2>test branch 1000</h2>
-    </>
+<>
+	<div>
+		<form>
+      <h1>LOGIN</h1>
+			<span>Please enter your login and password!</span>
+			<input type="username" placeholder="username" />
+			<input type="password" placeholder="Password" />
+			<a href="#">Forgot your password?</a>
+			<button>Sign In</button>
+      <span>Don’t have an account? <a href="#">Sign up</a></span>
+		</form>
+	</div>
+
+  <div>
+				<h1>Welcome Back!</h1>
+				<p>To keep connected with us please login with your personal info</p>
+				<button>Sign In</button>
+	</div>
+</>
   )
 }
 
